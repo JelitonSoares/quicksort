429d093bd8fccacdadb63081a18bb345421fa51e
jeliton soares <jelitonn.soarres@gmail.com>
2023-06-19 10:36:31 -0300

    Atualizando novos clientes

--- clientes.js
+++ clientes.js
@@ -4,37 +4,41 @@ const clientes = [
         idade: 59,
     },
     {
-        nome: 'Jeliton',
-        idade: 21,
-    },
-    {
-        nome: 'Elias',
-        idade: 58,
-    },
-    {
         nome: 'Vitor',
         idade: 22,
-    },
-    {
-        nome: 'Elisa',
-        idade: 26,
-    },
-    {
+   },
+   {
         nome: 'Jerlane',
         idade: 20,
-    },
-    {
-        nome: 'Terezinha',
-        idade: 52,
-    },
-    {
+   },
+   {
         nome: 'Celia',
-        idade: 59,
-    },
-    {
+        idade:  59,
+   },
+   {
         nome: 'Mazinha',
         idade: 44,
-    }
+   },
+   {
+        nome: 'Elisa',
+        idade: 26,
+   },
+   {
+        nome: 'Jeliton',
+        idade: 21,
+   },
+   {
+        nome: 'Terezinha',
+        idade: 53,
+   },
+   {
+        nome: 'Elias',
+        idade: 58,
+   },
+   {
+        nome: 'Julia',
+        idade: 22,
+   }
 ]
 
 module.exports = { clientes };
